0877636a0cf1552cded6a2bfb600f4d6498a2aa5
wzhzzmzzy <wzhzzmzzy@gmail.com>
2023-10-02 18:11:51 +0800

    Chaptor 29: Superclasses

--- src/debug.c
+++ src/debug.c
@@ -113,9 +113,11 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   case OP_SET_UPVALUE:
     return byteInstruction("OP_SET_UPVALUE", chunk, offset);
   case OP_GET_PROPERTY:
-      return constantInstruction("OP_GET_PROPERTY", chunk, offset);
+    return constantInstruction("OP_GET_PROPERTY", chunk, offset);
   case OP_SET_PROPERTY:
     return constantInstruction("OP_SET_PROPERTY", chunk, offset);
+  case OP_GET_SUPER:
+    return constantInstruction("OP_GET_SUPER", chunk, offset);
   case OP_POP:
     return simpleInstruction("OP_POP", offset);
   case OP_GREATER:
@@ -146,6 +148,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     return byteInstruction("OP_CALL", chunk, offset);
   case OP_INVOKE:
     return invokeInstruction("OP_INVOKE", chunk, offset);
+  case OP_SUPER_INVOKE:
+    return invokeInstruction("OP_SUPER_INVOKE", chunk, offset);
   case OP_CLOSURE: {
     offset++;
     uint8_t constant = chunk->code[offset++];
@@ -169,6 +173,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     return simpleInstruction("OP_RETURN", offset);
   case OP_CLASS:
     return constantInstruction("OP_CLASS", chunk, offset);
+  case OP_INHERIT:
+    return simpleInstruction("OP_INHERIT", offset);
   case OP_METHOD:
     return constantInstruction("OP_METHOD", chunk, offset);
   default:
